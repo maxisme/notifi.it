8345ccebe3d2df649d4b4c5a0ac80083d6696f1d
Maximilian Mitchell <max@maxis.me>
2020-02-03 20:40:21 +0000

    Move from PHP to Golang

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/maxisme/appserver"
+)
+
+func main() {
+	conf := appserver.ProjectConfig{
+		Name: "notifi",
+		Host: "notifi.it",
+		DmgPath: "notifi.dmg",
+		KeyWords: "notifi, notifi it, notify, notification, push notification, curl, mac, osx, mac to mac",
+		Description: "Send simple push notifications to your Mac using HTTP.",
+		Recaptcha: appserver.Recaptcha{
+			Pub: "LcbhkgUAAAAAIe6ZSvN9SQ8KaWgLb56xVUK82Ds",
+			Priv: "6LcbhkgUAAAAABuigVIcuHf_9DwOCcifC-ny_IwQ",
+		},
+		Sparkle: appserver.Sparkle{
+			Version:11,
+			Description:"foo",
+		},
+	}
+
+	if err := appserver.Serve(conf); err != nil {
+		panic(err)
+	}
+}
