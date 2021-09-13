ab25e8254600e1a65403ad0006a4550ccfdfdb69
Maximilian Mitchell <max@max.me.uk>
2021-09-13 14:46:08 +0100

    Add linux download

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 		Host:          "notifi.it",
 		GithubDmgRepo: "maxisme/notifi",
 		KeyWords:      "notifi, notifi it, notify, notification, push notification, curl, mac, osx, mac to mac, ios, flutter",
-		Description:   "Send simple push notifications to your MacOS and iOS devices using HTTP.",
+		Description:   "Send simple push notifications to your devices using HTTP.",
 		Recaptcha: appserver.Recaptcha{
 			Pub:  os.Getenv("captchpub"),
 			Priv: os.Getenv("captchpriv"),
